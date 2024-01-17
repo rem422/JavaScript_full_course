f2778d7fabd2659fb952c8182315c5c0b737639a
Rem Simiyu <84072712+rem422@users.noreply.github.com>
2024-01-17 12:50:27 +0300

    JavaScript Full Course

--- fundamental_part2/while_loop.js
+++ fundamental_part2/while_loop.js
@@ -1,1 +1,18 @@
- 
+// for(let rep = 1; rep <= 10; rep++) {
+//     console.log(`Lifting weights repetition ${rep} 🏋️`);
+// }
+
+let rep = 1;
+while(rep <= 10) {
+    // console.log(`WHILE: Lifting weights repetition ${rep} 🏋️`);
+    rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+    console.log(`You roled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+
+    if(dice === 6) console.log('Loop is about to end...');
+}
