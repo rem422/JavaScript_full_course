c971ffd299a2792a6dc1e8b4127072e0a72f1e71
Rem Simiyu <84072712+rem422@users.noreply.github.com>
2024-01-23 14:27:27 +0300

    JavaScript Full Course

--- developer_skills/Fixing_bugs.js
+++ developer_skills/Fixing_bugs.js
@@ -1,14 +1,15 @@
 const measureKelvin = () => {
-    const measurement = {
-        type: 'temp',
-        unit: 'celsius',
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
 
-        //C) FIXED IDENTIFY THE BUG
-        value: Number(prompt('Degrees Celsius:')),
-};
+    //C) FIXED IDENTIFY THE BUG
+      // value: Number(prompt('Degrees Celsius:')),
+    value: 10
+  };
 
-//B) FIND THE BUG
-    console.table(measurement);
+  //B) FIND THE BUG
+  console.table(measurement);
 
   // console.log(measurement.value);
   // console.warn(measurement.value);
@@ -16,9 +17,30 @@ const measureKelvin = () => {
 
   const kelvin = measurement.value + 273;
   return kelvin;
-}
+};
 // A) IDENTIFY THE BUG
 console.log(measureKelvin());
 
 
+// Using a debugger
+const calcTempAmplitudeBug = (t1, t2) => {
+  const temps = t1.concat(t2);
+  console.log(temps);
 
+  let max = 0;
+  let min = 0;
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 0, 5]);
+// A) IDENTIFY THE BUG
+console.log(amplitudeBug);
