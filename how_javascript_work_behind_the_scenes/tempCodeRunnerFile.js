17c519610a0daa51c86757742be7a004ba60f56d
Rem Simiyu <remsimiyu@gmail.com>
2024-03-22 15:13:59 +0300

    JavaScript Full Course

---
+++ how_javascript_work_behind_the_scenes/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+const calcAge = function(birthYear) {
+//     console.log(new Date().getFullYear() - birthYear);
+//     console.log(this);
+// }
+
+// calcAge(2000);
--- how_javascript_work_behind_the_scenes/thisKeyWord.js
+++ how_javascript_work_behind_the_scenes/thisKeyWord.js
@@ -12,16 +12,36 @@
 
 console.log(this);
 
-const calcAge = function(birthYear) {
-    console.log(new Date().getFullYear() - birthYear);
-    console.log(this);
-}
+// const calcAge = function(birthYear) {
+//     console.log(new Date().getFullYear() - birthYear);
+//     console.log(this);
+// }
 
-calcAge(2000);
+// calcAge(2000);
 
 const calcAgeArrow = (birthYear) => {
     console.log(new Date().getFullYear() - birthYear);
     console.log(this);
 }
 
-calcAgeArrow(1996);
+// calcAgeArrow(1996);
+
+const remmy = {
+    year: 2000,
+    calcAge: function() {
+        console.log(this);
+        return new Date().getFullYear() - this.year;
+    }
+}
+remmy.calcAge();
+
+const matilda = {
+    year: 2017,
+};
+
+matilda.calcAge = remmy.calcAge;
+matilda.calcAge();
+
+const f = remmy.calcAge;
+
+console.log(f());
